4496ce681b92c6d0231d15c148832ed35cf9f019
IsuruGodakanda <isurugreatcode@gmail.com>
2022-08-01 09:37:39 +0530

    MM-004-Add-create-user-service
    - Introduce got API handling library

--- api-gateway/src/adapters/UserService.js
+++ api-gateway/src/adapters/UserService.js
@@ -1,21 +1,12 @@
-import axios from "axios";
+import got from "got";
 
 const USERS_SERVICE_URI = "http://users-service:7101";
 
 export default class UsersService {
   static async createUser({ email, password }) {
-    const body = await axios
-      .post(`${USERS_SERVICE_URI}/users`, {
-        email,
-        password,
-      })
-      .then(function (response) {
-        return response.data;
-      })
-      .catch(function (error) {
-        return error;
-      });
-
+    const body = await got
+      .post(`${USERS_SERVICE_URI}/users`, { json: { email, password } })
+      .json();
     return body;
   }
 }
